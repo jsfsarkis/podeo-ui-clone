68ea99e8938ee74892391123523654fac0e3f451
Joseph Sarkis <jsfsarkis@gmail.com>
2022-04-07 13:24:45 +0300

    add image in profile screen

--- lib/ui/screens/profile_screen.dart
+++ lib/ui/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
 import 'package:podeo_ui_clone/ui/screens/login_screen.dart';
 import 'package:podeo_ui_clone/ui/screens/sign_up_screen.dart';
@@ -14,67 +15,105 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 100),
-          child: Column(
-            children: [
-              Center(
-                child: Text(
-                  'Podeo',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 80,
-                    fontWeight: FontWeight.bold,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipPath(
+                  clipper: TriangleClipper(),
+                  child: Image.asset('assets/images/muse-3.jpeg').blurred(
+                    blur: 0.5,
+                    blurColor: Colors.transparent,
                   ),
                 ),
-              ),
-              SizedBox(height: 65),
-              Center(
-                child: Text(
-                  'Unlimited Podcasts.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                const Positioned(
+                  top: 50,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: Text(
+                      'Podeo',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 80,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              const Center(
-                child: Text(
-                  'Free Forever.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                const Positioned(
+                  top: 250,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: Text(
+                      'Unlimited Podcasts.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 50),
-              AuthButton(
-                color: AppColors.accentColor,
-                text: 'SIGN UP FREE',
-                onTap: () {
-                  Navigator.pushNamed(context, SignUpScreen.id);
-                },
-              ),
-              const SizedBox(height: 20),
-              AuthButton(
-                color: AppColors.secondaryColor,
-                text: 'LOGIN',
-                onTap: () {
-                  Navigator.pushNamed(context, LoginScreen.id);
-                },
-              ),
-              const SizedBox(height: 50),
-              const SocialSignIn(),
-            ],
-          ),
+                const Positioned(
+                  top: 300,
+                  left: 0,
+                  right: 0,
+                  child: Center(
+                    child: Text(
+                      'Free Forever.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            AuthButton(
+              color: AppColors.accentColor,
+              text: 'SIGN UP FREE',
+              onTap: () {
+                Navigator.pushNamed(context, SignUpScreen.id);
+              },
+            ),
+            const SizedBox(height: 20),
+            AuthButton(
+              color: AppColors.secondaryColor,
+              text: 'LOGIN',
+              onTap: () {
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+            const SizedBox(height: 50),
+            const SocialSignIn(),
+          ],
         ),
       ),
     );
   }
+}
+
+class TriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(0, size.height - 70);
+    path.lineTo(size.width / 2, size.height);
+    path.lineTo(size.width, size.height - 70);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(TriangleClipper oldClipper) => false;
 }
